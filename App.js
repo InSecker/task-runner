5979113b5435c9eaa9ae2dcd71a8757276c3f026
InSecker <pierresecher98@gmail.com>
2021-04-19 15:46:27 +0200

    begining

--- App.js
+++ App.js
@@ -1,12 +1,14 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import UserList from './src/app/components/UserList';
+import {fetchAPI} from './src/app/utils/fetch';
+import {configuration as config} from './config/index.js';
 
 export default function App() {
+    const user = fetchAPI(config.endpoints.users);
     return (
         <View style={styles.container}>
-            <Text>Open up App.js to start working on your app!</Text>
-            <StatusBar style="auto"/>
+            <UserList data={user}/>
         </View>
     );
 }
