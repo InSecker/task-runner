c4a5b6be979a2f82bdc806f023af34543c29df88
Yahia <yahia@chefing.fr>
2021-04-19 15:15:42 +0200

    searchbar + style

--- App.js
+++ App.js
@@ -1,26 +1,24 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, { useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import { fetchAlbumsByUserId, fetchTodosByUserId, fetchUsers } from "./src/app/services/jsonplaceholder.service";
+import { configuration as api } from "./config";
+import SearchBar from "./src/app/components/SearchBar";
+import { fetchAPI } from "./src/app/utils/fetch";
 
 export default function App() {
+    const [searchText, setSearchText] = useState('');
 
-
-    fetchUsers().then(users => {
+    fetchAPI(api.endpoints.users).then(users => {
         console.log('users : ', users);
     });
 
-    fetchTodosByUserId(1).then(todos => {
-        console.log('todos : ', todos);
-    });
-
-    fetchAlbumsByUserId(1).then(albums => {
-        console.log('albums : ', albums);
-    });
+    const getSearchedMovies = (searchedText) => {
+        console.log('je chercher le user : ', searchedText);
+    }
 
     return (
         <View style={styles.container}>
-            <Text>Open up App.js to start working on your app!</Text>
+            <SearchBar searchText={searchText} onSearch={(searchedText) => getSearchedMovies(searchedText)}/>
             <StatusBar style="auto"/>
         </View>
     );
