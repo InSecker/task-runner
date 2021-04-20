1ebc5d056f74364702a502fdd636ff2a02ea28b3
Moity <anthony.77x@gmail.com>
2021-04-20 09:57:20 +0200

    add TodoCard and TodoMore to TodoList

--- App.js
+++ App.js
@@ -9,7 +9,6 @@ export default function App() {
     return (
         <View style={styles.container}>
             <StatusBar style="auto"/>
-            <TodoMore/>
             <TodoList/>
         </View>
     );
--- src/app/components/TodoList.js
+++ src/app/components/TodoList.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { View } from 'react-native';
 import TodoCard from './TodoCard';
+import TodoMore from './TodoMore';
 
 
 const TodoList = () => {
     return (
         <View>
+            <TodoMore/>
             <TodoCard/>
         </View>
     )
